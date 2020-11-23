b3fcdf5da8305f7123e9df858ed297684d5f21e8
tquyen2109 <t.quyen1997@yahoo.com>
2020-11-23 17:52:35 -0500

    Add FluentValidation package (API)

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,6 @@
 ﻿
 using Application.Activities;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -22,7 +23,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(cfg => {
+                cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
