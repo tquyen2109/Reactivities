a315a49fd76fac4a8119a8bc48816aabe52a87ca
tquyen2109 <t.quyen1997@yahoo.com>
2020-12-19 21:24:02 -0500

    Upload Images widget  (Client)

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { IActivity } from '../models/activity';
-import { IProfile } from '../models/profile';
+import { IPhoto, IProfile } from '../models/profile';
 import { IUser, IUserFormValues } from '../models/user';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
@@ -37,7 +37,14 @@ const requests = {
     get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
     post: (url:string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
     put: (url: string, body:{}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    delete: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody)
+    delete: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+    postForm: (url: string, file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post(url, formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        }).then(responseBody)
+    }
 }
 
 const Activities = {
@@ -55,7 +62,10 @@ const User = {
     register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
 }
 const Profile = {
-    get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`)
+    get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
+    uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo),
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id:string) => requests.delete(`/photos/${id}`)
 }
 export default {
     Activities,
--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -15,6 +15,7 @@ const ProfileContent = () => {
             menu={{fluid: true, vertical: true}}
             menuPosition='right'
             panes = {panes}
+            activeIndex={1}
         />
     )
 }
