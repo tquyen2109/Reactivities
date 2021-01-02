b2a3e78f856e03adee7723f302abded0c2bd2aed
tquyen2109 <t.quyen1997@yahoo.com>
2021-01-02 15:31:47 -0600

    Custom URL environment

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -5,7 +5,7 @@ import { IActivity, IActivityEnvelope } from '../models/activity';
 import { IPhoto, IProfile } from '../models/profile';
 import { IUser, IUserFormValues } from '../models/user';
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.interceptors.request.use((config) => {
     const token = window.localStorage.getItem('jwt');
     if(token) config.headers.Authorization = `Bearer ${token}`;
@@ -17,7 +17,7 @@ axios.interceptors.response.use(undefined, error => {
     if(error.message === 'Network Error' && !error.response) {
         toast.error('API is not running');
     }
-    const {status,data, config, headers} = error.response;
+    const {status,data, config} = error.response;
     if(status === 404) {
         history.push('/notfound');
     }
@@ -34,13 +34,11 @@ axios.interceptors.response.use(undefined, error => {
     throw error.response;
 })
 const responseBody = (response: AxiosResponse) => response.data;
-const sleep = (ms: number) => (response: AxiosResponse) => 
-    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response),ms));
 const requests = {
-    get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-    post: (url:string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-    put: (url: string, body:{}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
-    delete: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url:string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body:{}) => axios.put(url, body).then(responseBody),
+    delete: (url: string) => axios.delete(url).then(responseBody),
     postForm: (url: string, file: Blob) => {
         let formData = new FormData();
         formData.append('File', file);
@@ -51,7 +49,7 @@ const requests = {
 }
 
 const Activities = {
-    list: (params: URLSearchParams): Promise<IActivityEnvelope> => axios.get('/activities', {params: params}).then(sleep(1000)).then(responseBody),
+    list: (params: URLSearchParams): Promise<IActivityEnvelope> => axios.get('/activities', {params: params}).then(responseBody),
     detail:(id: string) => requests.get(`/activities/${id}`),
     create:(activity: IActivity) => requests.post('/activities', activity),
     update: (activity: IActivity) => requests.put(`/activities/${activity.id}`, activity),
